6bab3dea81a834165dcdcaae25d964dc6b8a4d4c
vertskater <vertskater@gmx.at>
2024-02-20 13:44:23 +0100

    Add Destroy component and update Delete component

--- src/Movie.tsx
+++ src/Movie.tsx
@@ -1,46 +1,44 @@
 import { Form, useLoaderData } from "react-router-dom";
 import { getMovie } from "./handleMovies";
 
-export async function loader ({params}:{params:{id:string}}){
-    const movie = await getMovie(params.id);
-    return {movie};
+export async function loader({ params }: { params: { id: string } }) {
+  const movie = await getMovie(params.id);
+  return { movie };
 }
 
-type MovieType ={
-    id:string;
-    title:string;
-    runtime:number;
-    img:string;
-    social: string;
+type MovieType = {
+  id: string;
+  title: string;
+  runtime: number;
+  img: string;
+  social: string;
 };
 
 export default function Movie() {
-    const { movie } = useLoaderData() as { movie: MovieType }
+  const { movie } = useLoaderData() as { movie: MovieType };
 
-    return (
-        <div id="movie">
-            <div>
-                <img height={300} src={movie.img} key={movie.img} alt={movie.title} />
-            </div>
-            <div>
-                <h1>{movie.title ? movie.title : "no title"}</h1>
-                <i>{movie.runtime && `Runtime: ${movie.runtime} Min.`}</i>
-                {movie.social && (
-                    <p>
-                        <a target="_blank" href={`https://www.twitter.com/${movie.social}`}>
-                         {movie.social}   
-                        </a>
-                    </p>
-                )}
-               
-                    <Form action='edit'>
-                        <button type="submit">Edit</button>
-                    </Form>
-                    <Form action='destroy'>
-                        <button type="submit">Delete</button>
-                    </Form>
-               
-            </div>
-        </div>
-    );
+  return (
+    <div id="movie">
+      <div>
+        <img height={300} src={movie.img} key={movie.img} alt={movie.title} />
+      </div>
+      <div>
+        <h1>{movie.title ? movie.title : "no title"}</h1>
+        <i>{movie.runtime && `Runtime: ${movie.runtime} Min.`}</i>
+        {movie.social && (
+          <p>
+            <a target="_blank" href={`https://www.twitter.com/${movie.social}`}>
+              {movie.social}
+            </a>
+          </p>
+        )}
+        <Form action="edit">
+          <button type="submit">Edit</button>
+        </Form>
+        <Form method="delete" action="destroy">
+          <button type="submit">Delete</button>
+        </Form>
+      </div>
+    </div>
+  );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,38 +1,38 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
-import RootElement, {loader as rootloader, action as rootAction} from "./RootElement";
-import Movie, {loader as movieLoader} from "./Movie";
-import EditMovie, { action as editAction} from "./EditMovie";
-import Delete, {action as deleteAction} from "./Delete";
-
-export default function Router(){
-    const router=createBrowserRouter([
+import RootElement, {
+  loader as rootloader,
+  action as rootAction,
+} from "./RootElement";
+import Movie, { loader as movieLoader } from "./Movie";
+import EditMovie, { action as editAction } from "./EditMovie";
+import { action as deleteAction } from "./Destroy";
+export default function Router() {
+  const router = createBrowserRouter([
     {
-        path: "/",
-        element: <RootElement/>,
-        errorElement:<ErrorPage/>,
-        loader: rootloader,
-        action: rootAction,
-
-        children: [
-            {
-                path: "/movies/:id",
-                element: <Movie/>,
-                loader: movieLoader as any,
-            },
-            {
-                path: "/movies/:id/edit",
-                element: <EditMovie/>,
-                loader: movieLoader as any,
-                action: editAction as any,
-            },
-            {
-                path: "/movies/:id/destroy",
-                element: <Delete />,
-                action: deleteAction as any,
-            },
-        ],
+      path: "/",
+      element: <RootElement />,
+      errorElement: <ErrorPage />,
+      loader: rootloader,
+      action: rootAction,
+      children: [
+        {
+          path: "/movies/:id",
+          element: <Movie />,
+          loader: movieLoader as any,
+        },
+        {
+          path: "/movies/:id/edit",
+          element: <EditMovie />,
+          loader: movieLoader as any,
+          action: editAction as any,
+        },
+        {
+          path: "/movies/:id/destroy",
+          action: deleteAction as any,
+        },
+      ],
     },
-    ]);
-    return <RouterProvider router={router} />;
+  ]);
+  return <RouterProvider router={router} />;
 }
